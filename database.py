0d4002ba443e03536a8295e41456cf0ed9c20c01
Florian Niedermeier <florian-niedermeier@gmx.de>
2024-05-30 18:12:21 +0200

    removed "nextDay" columns and coressponding methods

--- database.py
+++ database.py
@@ -123,68 +123,6 @@ def main_db(rebuild):
                         if_sheet_exists="replace") as writer:
         df.to_excel(writer, sheet_name='Meta', index=False)
 
-
-def getPeakOfNextDay(dateOfEC, code):
-
-    nextDay = nextWorkdayAfterDays(dateOfEC, 1)
-
-    ticker = yf.Ticker(code)
-
-    try:
-        history = ticker.history(start=nextDay, end=nextDay + timedelta(days=1))
-    except:
-        return None
-
-    for i, day in history.iterrows():
-
-        open = round(day["Open"], 2)
-        lowest = round(day["Low"], 2)
-        highest = round(day["High"], 2)
-
-        return -round((open - highest) / open, 4), -round((open - lowest) / open, 4)
-
-
-def getCloseOfNextDay(dateOfEC, code):
-
-    nextDay = nextWorkdayAfterDays(dateOfEC, 1)
-
-    ticker = yf.Ticker(code)
-
-    try:
-        history = ticker.history(start=nextDay, end=nextDay + timedelta(days=1))
-    except:
-        return None
-
-    for i, day in history.iterrows():
-
-        open = round(day["Open"], 2)
-        close = round(day["Close"], 2)
-        return -round((open - close) / open, 4)
-
-
-def getChangeFromPrevCloseToOpen(dateOfEC, code):
-
-    previousDay = nextWorkdayAfterDays(dateOfEC, -1)
-
-    ticker = yf.Ticker(code)
-    history_dayBefore = ticker.history(start=previousDay - timedelta(days=1), end=dateOfEC)
-    history_dayOfEC = ticker.history(start=dateOfEC, end=dateOfEC + timedelta(days=1))
-
-    close = None
-    open = None
-
-    for i, day in history_dayBefore.iterrows():
-        close = round(day["Close"], 2)
-
-    for i, day in history_dayOfEC.iterrows():
-        open = round(day["Open"], 2)
-
-    if open and close:
-        return -round((close - open) / close, 4)
-    else:
-        return None
-
-
 def getRunUp(days, dateOfEC, code):
 
     startDate = nextWorkdayAfterDays(dateOfEC, -days)
@@ -216,7 +154,7 @@ def main_movement():
 
     for index, row in df.iterrows():
 
-        if not pd.isnull(df.loc[index, "nextDayLow"]):
+        if not pd.isnull(df.loc[index, "close"]):
             continue
 
         code = row["Code"]
@@ -237,27 +175,18 @@ def main_movement():
         try:
             if afterOrBeforeOpen == "vor":
                 movement_low, movement_high, movement_close = getMovementAfterOpening(dateOfEC, code)
-                nextDayPeak, nextDayLow = getPeakOfNextDay(dateOfEC, code)
-                nextDayClose = getCloseOfNextDay(dateOfEC, code)
             else:
                 movement_low, movement_high, movement_close = getMovementAfterOpening(dateOfEC + timedelta(days=1), code)
-                nextDayPeak, nextDayLow = getPeakOfNextDay(dateOfEC + timedelta(days=1), code)
-                nextDayClose = getCloseOfNextDay(dateOfEC + timedelta(days=1), code)
         except:
             continue
 
         df.at[index, "movementAfterOpening_low"] = movement_low
         df.at[index, "movementAfterOpening_high"] = movement_high
         df.at[index, "close"] = movement_close
-        df.at[index, "nextDayPeak"] = nextDayPeak
-        df.at[index, "nextDayLow"] = nextDayLow
-        df.at[index, "nextDayClose"] = nextDayClose
-        df.at[index, "prevCloseToOpen"] = getChangeFromPrevCloseToOpen(dateOfEC, code)
 
         print(index, code)
 
-    with pd.ExcelWriter("C:/Users/FSX-P/Aktienanalyse/Aktien.xlsx", engine='openpyxl', mode='a',
-                        if_sheet_exists="replace") as writer:
+    with pd.ExcelWriter("C:/Users/FSX-P/Aktienanalyse/Aktien.xlsx", engine='openpyxl', mode='a', if_sheet_exists="replace") as writer:
         df.to_excel(writer, sheet_name='ProfitForecastWelt_script', index=False)
 
 def main_runUp():
